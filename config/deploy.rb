12d8931e53983b0abdb2f5c9ccfd4aad64a7a41a
Andrey Smirnov <s.andrey.d@gmail.com>
2011-07-15 03:17:56 +0400

    Deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,32 +21,47 @@
 #   end
 # end
 
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Для работы rvm
-require 'rvm/capistrano' # Для работы rvm
-require 'bundler/capistrano' # Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика. 
+# RVM integration
+# http://beginrescueend.com/integration/capistrano/
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                               # Load RVM's capistrano plugin.
+set :rvm_ruby_string, 'ruby'                           # Or whatever env you want it to run in.
+set :rvm_type, :user                                   # Copy the exact line. I really mean :user here
+
+set :default_environment, {
+  'PATH' => "$HOME/.rvm/gems/ree/1.8.7/bin:/path/to/.rvm/bin:/path/to/.rvm/ree-1.8.7-2009.10/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.2',
+  'GEM_HOME'     => '$HOME/.rvm/gems/ruby-1.9.2-p180',
+  'GEM_PATH'     => '$HOME/.rvm/gems/ruby-1.9.2-p180',
+#  'BUNDLE_PATH'  => '$HOME/.rvm/gems/ruby-1.9.2-p180'  # If you are using bundler.
+}
+
+# http://gembundler.com/deploying.html
+# Automatic deployment with Capistrano
+require 'bundler/capistrano'
+
 
 set :application, "nwn"
 set :rails_env, "production"
 set :domain, "anti@firstvds"
 set :deploy_to, "/vault/production/#{application}"
+set :deploy_via, :remote_cache
 set :use_sudo, false
 set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
 set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-set :rvm_ruby_string, 'ruby' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
-set :rvm_type, :user # Указывает на то, что мы будем использовать rvm, установленный у пользователя, от которого происходит деплой, а не системный rvm.
 
 set :scm, :git # Используем git. Можно, конечно, использовать что-нибудь другое - svn, например, но общая рекомендация для всех кто не использует git - используйте git. 
 set :repository,  "git@github.com:advsm/nwn_quotes.git" # Путь до вашего репозитария. Кстати, забор кода с него происходит уже не от вас, а от сервера, поэтому стоит создать пару rsa ключей на сервере и добавить их в deployment keys в настройках репозитария.
 set :branch, "master" # Ветка из которой будем тянуть код для деплоя.
 #set :deploy_via, :remote_cache # Указание на то, что стоит хранить кеш репозитария локально и с каждым деплоем лишь подтягивать произведенные изменения. Очень актуально для больших и тяжелых репозитариев.
 
-role :web, domain
-role :app, domain
-role :db,  domain, :primary => true
+#role :web, "nwn.name"
+role :app, "62.109.21.110"
+#role :db,  "nwn.name", :primary => true
 
 after 'deploy:update_code', :roles => :app do
-  # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
+  # change mongoid.yml with symlink to shared libraries
   run "rm -f #{current_release}/config/mongoid.yml"
   run "ln -s #{deploy_to}/shared/config/mongoid.yml #{current_release}/config/mongoid.yml"
 end
