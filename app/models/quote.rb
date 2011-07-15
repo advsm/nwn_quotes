0c0f851f44b5c6826990bc80f360c38c4883ff80
Andrey Smirnov <s.andrey.d@gmail.com>
2011-07-15 19:52:09 +0400

    Validation for ident and quote content

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -14,5 +14,9 @@ class Quote
 	field :content, type: String
 	field :approved_at, type: DateTime, default: nil
 	belongs_to :user
+	
+	validates_presence_of :content
+	validates_length_of :content, minimum: 10, maximum: 2000
+	validation_uniqueness_of :ident
 end
 
